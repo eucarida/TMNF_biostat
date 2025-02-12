dde983a41b88493c7971cfa0c6985c81bcd2e9f7
eucardia <olle.heyman@outlook.com>
2025-02-12 01:31:23 +0100

    Converted .gbx to csv, loaded libraries and created GAS colum to signal acceleration/break and releas

---
+++ TMNF_biostat_A01wr.R
@@ -0,0 +1,14 @@
+## created on 2025-02-12 by eucardia
+## LAST UPDATED: 2025-02-12
+## This is project exists to merge statistics with TMNF wr in comparison with 
+## tas runs and other runs
+
+# clear memory
+rm(list = ls())
+
+
+#load libraries
+library(performance)
+library(tidyverse)
+
+# load wr for A01
