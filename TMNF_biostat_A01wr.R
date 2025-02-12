bb61f39436e49494240d353b2f64662a068e9244
eucardia <olle.heyman@outlook.com>
2025-02-12 01:37:23 +0100

    forgot to save befor bublishing last one

--- TMNF_biostat_A01wr.R
+++ TMNF_biostat_A01wr.R
@@ -11,4 +11,28 @@ rm(list = ls())
 library(performance)
 library(tidyverse)
 
-# load wr for A01
+# load wr for A01 ####
+## IMPORTANT: to load the data from the run/race the .gbx file needs to be
+## converted into a .csv (comma delimited). To do this the the online tool
+## "https://io.gbx.tools/extract-inputs-tmi" convert .gbx > .txt
+## Then open the .txt in Excel using space delimited seperation
+## In Excel heders are placed and the removal of start and finish data is
+## done. Then the file is saved as a comma delimited .csv file
+## tidy shit is to ensue
+
+df_edd_raw <- read_csv2("A01-Race_eddieman194.csv")
+
+# make df_edd_raw tidy ####
+## TO DO ##
+  # seperate value into stering value and accelerate/breake
+  # remove "press" and "rel"from INPUT
+  # fill in missing information (like lack of stering input at TIME = 0) 
+
+print.data.frame(df_edd_raw)
+
+df_edd_raw %>% 
+  mutate(GAS = VALUE)
+  
+
+
+
